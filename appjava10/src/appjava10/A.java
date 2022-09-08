e0cc7f8a55cfa5f89658de5a329ca09de7d0fe7b
raman <raman@LAPTOP-EM7M97SB>
2022-09-08 22:14:48 +0530

    first commit

---
+++ appjava10/src/appjava10/A.java
@@ -0,0 +1,9 @@
+package appjava10;
+
+public class A {
+	int i =10;
+	public void Test() {
+		System.out.println("from Test");
+	}
+
+}
---
+++ appjava10/src/appjava10/B.java
@@ -0,0 +1,10 @@
+package appjava10;
+
+public class B extends A {
+	public static void main(String[] args) {
+		B b1 = new B();
+		b1.Test();
+		System.out.println(b1.i);
+	}
+
+}
